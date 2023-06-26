86756d061fceca85f439da19ecb55d92f17ef971
Moharaa <137526332+Moharaa@users.noreply.github.com>
2023-06-26 19:44:17 +0330

    Create Rota.c

---
+++ Rota.c
@@ -0,0 +1,28 @@
+<include <iostream#
+<include <cmath#
+;using namespace std
+()int main
+}
+;double a,b,c
+;cin>>a>>b>>c
+;double delta=pow(b,2)-4*a*c
+    if (delta>0)
+    }
+;cout<<"2 answers:"<<endl
+;double x1=(-b+sqrt(delta))/(2*a)
+;double x2=(-b-sqrt(delta))/(2*a)
+;cout<<x1<<endl<<x2<<endl
+     {
+    else if(delta==0)
+ } 
+;cout<<"1 answers:"<<endl
+;double x1=(-b)/2*a
+;cout<<x1<<endl
+{
+    else
+    }
+;cout<<"No answer!"<<endl
+  {
+;return 0
+
+{
